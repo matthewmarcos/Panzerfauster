5f1777d6317bff906d12d87cfd3df718df5e232e
Matthew Marcos <matthewmarcos94@gmail.com>
2016-11-02 09:13:39 +0800

    JTextField

--- Client/Client.java
+++ Client/Client.java
@@ -4,20 +4,21 @@ import javax.swing.*;
 public class Client extends JFrame {
 
     public static final Dimension DIMENSION = new Dimension(800, 600);
+    public static final String APP_NAME = new String("Panzerfauster");
     private static Container cont;
     private Game currentGame;
 
     public Client() throws Exception {
-        super("TankDerby");
+        super(APP_NAME);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(Client.DIMENSION);
-        this.setVisible(true);
+
         cont = this.getContentPane();
         cont.setSize(Client.DIMENSION);
-
         currentGame = new Game();
-
-        cont.removeAll();
         cont.add(currentGame);
+
+        this.setVisible(true);
+        // cont.removeAll();
     }
 }
--- Client/Game.java
+++ Client/Game.java
@@ -4,13 +4,16 @@ import javax.swing.*;
 public class Game extends JPanel {
 
     private void initialize() {
+        this.setLayout(new BorderLayout());
+        this.setSize(Client.DIMENSION);
+
         System.out.println("Set up the values of the game here.");
+        JTextField chatbar = new JTextField();
+        this.add(chatbar, BorderLayout.SOUTH);
     }
 
     public Game() {
         super();
-        this.setLayout(null);
-        this.setSize(Client.DIMENSION);
         initialize();
     }
 }
