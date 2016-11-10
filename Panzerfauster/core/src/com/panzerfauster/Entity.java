2a8b32914a6db1203ea5235467604aa20a92cd6c
Matthew Marcos <matthewmarcos94@gmail.com>
2016-11-10 11:40:54 +0800

    Paint sprites with Sprite class

--- Panzerfauster/core/src/com/panzerfauster/Entity.java
+++ Panzerfauster/core/src/com/panzerfauster/Entity.java
@@ -11,21 +11,45 @@ import com.badlogic.gdx.graphics.Texture;
 public class Entity {
 
     protected Texture image;
+    protected int xcoord, ycoord;
 
-    public Entity(
-        String image_path, // Location of image
-        boolean isEnemy, // Will it be hostile?
-        String name,
-        int xcoordinate, //xLocation on screen
-        int ycoordinate  //yLocation on screen
+    protected Entity(
+            String image_path, // Location of image
+            boolean isEnemy, // Will it be hostile?
+            String name,
+            int xcoordinate, //xLocation on screen
+            int ycoordinate //yLocation on screen
     ) {
         try {
             this.image = new Texture(Gdx.files.internal(image_path));
-
+            this.xcoord = xcoordinate;
+            this.ycoord = ycoordinate;
         }
         catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public int getX() {
+//  Returns the x coordinate to draw the image
+        return xcoord - image.getHeight() / 2;
+    }
+
+    public int getY() {
+//  Returns the y coordinate to draw the image
+        return ycoord - image.getHeight() / 2;
+    }
+
+    public int getXpos() {
+        return this.xcoord;
+    }
+
+    public int getYpos() {
+        return this.ycoord;
+    }
+
+    public Texture getImage() {
+//  Returns the image that represents this entity
+        return this.image;
+    }
 }
--- Panzerfauster/core/src/com/panzerfauster/Panzerfauster.java
+++ Panzerfauster/core/src/com/panzerfauster/Panzerfauster.java
@@ -2,32 +2,49 @@ package com.panzerfauster;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Panzerfauster extends ApplicationAdapter {
+public class Panzerfauster extends ApplicationAdapter implements InputProcessor {
+    private int winWidth, winHeight;
 	private SpriteBatch batch;
 	private BitmapFont font;
-	private Texture playerSprite;
+	private Sprite player;
+	private float xPos, yPos, speed;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		playerSprite = new Texture(Gdx.files.internal("sprites/Tank.png"));
+		Texture temp = new Texture(Gdx.files.internal("sprites/Tank.png"));
+        player = new Sprite(temp);
         font = new BitmapFont();
         font.setColor(Color.RED);
+
+        this.winWidth = Gdx.graphics.getWidth();
+        this.winHeight = Gdx.graphics.getHeight();
+        this.speed = 10;
+
+        xPos = winWidth/2 - temp.getWidth()/2;
+        yPos = winHeight/2 - temp.getWidth()/2;
+        player.setPosition(xPos, yPos);
     }
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0.1f, 1, 0.1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(playerSprite, 0, 0);
-//        font.draw(batch, "Hello World", 200, 200);
+
+        player.setPosition(xPos, yPos);
+
+        batch.begin();
+        //batch.draw(playerSprite, x, y);
+        player.draw(batch);
         batch.end();
     }
 	
@@ -35,6 +52,48 @@ public class Panzerfauster extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
         font.dispose();
-        playerSprite.dispose();
 	}
+
+    @Override
+    public boolean keyDown(int keycode) {
+
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        System.out.println("character");
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
